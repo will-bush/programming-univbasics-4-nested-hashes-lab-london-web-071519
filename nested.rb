80ba467849a82d88b165b4cdd39075b016930509
Will Bush <williamdcbush@gmail.com>
2019-07-01 17:08:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,8 +102,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
      
 end
